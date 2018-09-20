65fff232a37db81a29ca43afaf571a7423c3f931
Aubrey O'Neal <8096990+aubrilya@users.noreply.github.com>
2018-09-19 19:25:00 -0500

    Add files via upload

---
+++ example.js
@@ -0,0 +1,26 @@
+var warrantsData = [
+ {
+   "ID": 1,
+   "State": "Ohio",
+   "NumberOfWarrants": 230130,
+   "OutstandingWarrants": 1203,
+   "RequiresCops": "Yes",
+   "DataAccessed": "Jan. 13, 2018"
+ },
+ {
+   "ID": 2,
+   "State": "Texas",
+   "NumberOfWarrants": 1143912,
+   "OutstandingWarrants": 99362,
+   "RequiresCops": "No",
+   "DataAccessed": "Jan. 28, 2018"
+ },
+ {
+   "ID": 3,
+   "State": "Florida",
+   "NumberOfWarrants": 563,
+   "OutstandingWarrants": 12,
+   "RequiresCops": "Yes",
+   "DataAccessed": "Feb. 29, 2018"
+ }
+]
